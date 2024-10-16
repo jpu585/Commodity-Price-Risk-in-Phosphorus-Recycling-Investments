12e29cfa1af3158fe88bdf3368e63cf7d7dc7ade
jpu585 <75808277+jpu585@users.noreply.github.com>
2024-10-16 10:46:46 +0200

    Add files via upload

---
+++ 00_master.R
@@ -0,0 +1,17 @@
+ #Commodity Price Risk in Phosphorus Recycling Investments
+#
+# Supplementary R code for main results
+# 
+# Jan-Philip R. Uhlemann, Alfons Oude Lansink, James J. Leahy, Jean-Paul Chavas, Tobias Dalhaus
+#
+# Corresponding author: Jan-Philip R. Uhlemann (jan-philip.uhlemann@wur.nl)
+#
+# Please check our github repository for more codes and information
+# https://github.com/jpu585/Commodity-Price-Risk-in-Phosphorus-Recycling-Investments
+#
+# The first script uses the World Bank Commodity Price Data (The Pink Sheet)
+# and produces a csv file with the relevant price, price index and date variables
+# setwd()
+source("01_data_read.R")
+# The second script reproduces the results of the analysis in the manuscript
+source("02_main_analysis.R")
